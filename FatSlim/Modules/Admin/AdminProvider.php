0138d82a159de554dc750885859de3277b2967e6
Sc3ne <gokaysolman@hotmail.com>
2016-10-12 04:50:46 +0300

    module functions added

--- FatSlim/Modules/Admin/AdminProvider.php
+++ FatSlim/Modules/Admin/AdminProvider.php
@@ -7,7 +7,10 @@ class AdminProvider extends ModuleProvider implements ModuleProviderInterface {
 
 	public function register() {
 		
+		$this->setDir(__DIR__);
 		$this->setViewPrefix('admin');
+		$this->setViewPath(__DIR__ .'/Views');
+		$this->setRoutes(__DIR__ .'/routes.php');
 		
 	}
 
